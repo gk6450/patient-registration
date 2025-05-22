f46d91b1ddaf8465ae175dcb443d6b837b1e7d5b
Goutham Kanagarasu <gouthamgk2016@gmail.com>
2025-05-22 23:39:01 +0530

    Updated db/index.jsto use matching dataDir for PGlite persistence

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { live } from '@electric-sql/pglite/live';
 const pg = new PGliteWorker(
     new Worker(new URL('./worker.js', import.meta.url), { type: 'module' }),
     {
-        dataDir: 'idb://my-patient-db',
+        dataDir: 'idb://patient-db',
         extensions: { live }
     }
 );
